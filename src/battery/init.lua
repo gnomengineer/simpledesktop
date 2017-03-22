50e453d3ffd245a62c231a66aa8cc0552fbc23f2
troopa <troopa.der.pate@gmail.com>
2017-03-22 17:28:47 +0100

    closed the file streams opened by the battery check

--- src/battery/init.lua
+++ src/battery/init.lua
@@ -6,32 +6,38 @@ local function trim(s)
 end
 
 local function readIO(path)
-    return assert(io.open("/sys/class/power_supply/BAT0/"..path,"r"))
+    local f = assert(io.open("/sys/class/power_supply/BAT0/"..path,"r"))
+    local input = file:read("*all")
+    file:close()
+    return input
 end
 
 local function getString(path)
-    return trim(readIO(path):read("*all"))
+    return trim(readIO(path))
 end
 
 local function getNumber(path)
-    return tonumber(readIO(path):read("*all"))
+    return tonumber(readIO(path))
 end
 
 function battery.checkBattery()
-    local capacity = getNumber("capacity")
-    local status = getString("status")
-    local power = getNumber("power_now")
-    local energy = getNumber("energy_now")
+    if getString("status") == "Discharging" then
 
-    local time = energy / power * 60
-    local minutes = math.floor(time % 60)
-    local hours = math.floor((time - minutes) / 60)
+        local capacity = getNumber("capacity")
 
-    if status == "Discharging" and capacity <= 10 then
-        naughty.notify({ 
-            preset =  naughty.config.presets.critical,
-            title = "testoutput",
-            text = capacity .. "% " ..hours .. ":"..minutes .." remianing."})
+        if capacity <= 10 then
+            local power = getNumber("power_now")
+            local energy = getNumber("energy_now")
+
+            local time = energy / power * 60
+            local minutes = math.floor(time % 60)
+            local hours = math.floor((time - minutes) / 60)
+
+            naughty.notify({ 
+                preset =  naughty.config.presets.critical,
+                title = "testoutput",
+                text = capacity .. "% " ..hours .. ":"..minutes .." remianing."})
+        end
     end
 end
 
