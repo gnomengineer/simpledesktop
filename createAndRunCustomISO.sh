5a1d179826d751cd3cb77d434926c9a272be834a
gnomengineer <troopa.der.pate@gmail.com>
2019-04-24 23:24:13 +0200

    add a custom script to build the livecd files with customization options

---
+++ createAndRunCustomISO.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# applies custom settings, files and packages to the airootfs
+# then builds the ISO image using the copied livecd/build.sh
+
+if [ $# -ne 1 ];
+then
+    echo "requires the target output directory to copy the livecd template files into"
+    exit
+fi
+
+#cp achiso/releng
+cp -r /usr/share/archiso/configs/releng "$1/livecd"
+
+#patch customize-airootfs.sh
+cp customization-parts/customize_airootfs.sh "$1/livecd/airootfs/root/"
+
+#cp skel
+cp -r customization-parts/skel/ "$1/livecd/airootfs/etc/"
+
+#cp custom configs
+cp customization-parts/hostname customization-parts/locale.conf "$1/livecd/airootfs/etc/"
+
+#add custom packages
+cat customization-parts/packages.txt >> "$1/livecd/packages.x86_64"
+
+#prepare and run the build.sh
+sudo chown -R root:root "$1/livecd/"
+cd "$1/livecd/" && sudo ./build.sh -v
+
