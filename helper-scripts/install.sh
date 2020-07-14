14265182c87b35418fcfc23365498c3ddbccea3e
Daniel Foehn <troopa.der.pate@gmail.com>
2020-07-14 22:47:45 +0200

    add commands to format and partition the target device and mount them

--- helper-scripts/install.sh
+++ helper-scripts/install.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 
-# change $DEVICE according to your hard drive target
-DEVICE="sda"
-echo 'type=83' | sudo sfdisk /dev/$DEVICE
-mkfs.ext4 /dev/$DEVICE
-mount /dev/$DEVICE /mnt
+echo "You have to edit this install script or take a look at what it does"
+echo "if you have done so, remove the exit on the line below and run it (again)"
+exit
+
+# change $DEVICE according to your hard drive target (e.g. /dev/sda, /dev/nvme0n1)
+DEVICE="/dev/sda"
+# set this to false if you don't require UEFI boot
+UEFI="true"
+
+# erase previous MBR and GPT paritions
+sgdisk -Z $DEVICE
+
+# create an EFI partition (1:) and the main partition (2:)
+sgdisk -n 1:0:+512M $DEVICE
+sgdisk -n 2:0:+9G $DEVICE
+
+# change the types of previously created partitions
+if [ "$UEFI" == "true" ];
+then
+    sgdisk -t 1:ef00 $DEVICE
+else
+    sgdisk -t 1:8200 $DEVICE
+fi
+sgdisk -t 2:8300 $DEVICE
+
+# mount the partitions
+# CARE! in case your device is /dev/nvme0n1 put a 'p' in front of the number
+mount "${DEVICE}2" /mnt
+mkdir /mnt/boot/
+mount "${DEVICE}1" /mnt/boot
 
 rsync -aAXvP /* /mnt --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/lost+found,/.gvfs}
 cp -avT /run/archiso/bootmnt/arch/boot/$(uname -m)/vmlinuz /mnt/boot/vmlinuz-linux
